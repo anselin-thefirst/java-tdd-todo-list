aae79162eb753fc9be556dbd84f12e20c03cf2c3
Frida Anselin <frida.anselin@se.experis.com>
2025-01-09 23:41:41 +0100

    Add method updateTaskStatus to pass  testUpdateTaskStatusFalseToTrue - green

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -14,12 +14,20 @@ public class TodoList {
     }
 
     public String listTasks(HashMap<String, Boolean> tasks) {
-        String output = "";
+        String output;
         if (tasks.isEmpty()) {
             output = "Todo-list is empty";
             return output;
         }
         output = tasks.keySet().toString();
         return output;
+    }
+
+    public Boolean updateTaskStatus(HashMap<String, Boolean> tasks, String name) {
+        if (tasks.get(name).equals(false)) {
+            tasks.put(name, true);
+            return true;
+        }
+        return false;
     }
 }

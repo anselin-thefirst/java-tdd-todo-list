d47db52045f55c40d2e941f3708c4c517b5992ca
Frida Anselin <frida.anselin@se.experis.com>
2025-01-09 14:33:46 +0100

    testAddNewTaskToList red implementation

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,19 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 class TodoListTest {
+
+    HashMap<String, Boolean> tasks = new HashMap<>() {{
+        put("Tidy up", false);
+        put("Finish task", false);
+        put("Lunch", true);
+    }};
+
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testAddNewTaskToList() {
+        tasks.put("Sleep", false);
+        Assertions.assertTrue(true);
     }
 }
